84bd6cb72285ae09831c57e96efe7275a88e7132
Gokhan Ucar <gokhanucar10@gmail.com>
2020-10-19 00:20:05 +0300

    Dynamic routes

--- models/product.js
+++ models/product.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const rootDir = require('../util/path');
+
 const p = path.join(rootDir, 'data', 'products.json');
 
 const getProductsFromFile = cb => {
@@ -24,6 +25,7 @@ module.exports = class Product {
     }
 
     save() {
+        this.id = Math.random().toString();
         getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), err => {
@@ -34,5 +36,12 @@ module.exports = class Product {
 
     static fetchAll(cb) {
         getProductsFromFile(cb);
+    }
+
+    static findById(id, cb) {
+        getProductsFromFile(products => {
+            const product = products.find(p => p.id === id);
+            cb(product);
+        });
     }
 };
