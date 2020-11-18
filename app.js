92818754f71ec81a72dcd86cd5724cb3fc9c0c63
Gokhan Ucar <gokhanucar10@gmail.com>
2020-11-18 23:54:59 +0300

    mongoose

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
+
+process.env.TZ = 'Europe/Istanbul';
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -16,9 +20,37 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findById('5fb434f69bed731188cbb602')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+mongoose
+    .connect('mongodb+srv://admin:system@node-guide.b8c3j.mongodb.net/node-guide?retryWrites=true&w=majority')
+    .then(result => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'Gokhan',
+                    email: 'gokhan@test.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        })
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
